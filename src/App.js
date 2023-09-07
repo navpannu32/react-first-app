8cde17c63e8cdb9030eec88743806f277a34334f
navpannu32 <pannunav74@gmail.com>
2023-09-07 15:07:34 -0500

    building components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import Todo from './components/Todo';
+
 function App() {
-  return <div>
-    <h1>My Todos</h1>
-    <div className='card'>
-      <h2>TITLE</h2>
-      <div>
-        <button>Delete</button>
-      </div>
+  return (
+    <div>
+     <h1>My Todos</h1>
+     <Todo text='Learn React' />
+     <Todo text='Master React' />
+     <Todo text='Explore the full React course' />
     </div>
-  </div>;
+    );
 }
 
 export default App;
