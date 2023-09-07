82f984a5e8883e2af8c3aa730f3abd43ba8ff0ce
navpannu32 <pannunav74@gmail.com>
2023-09-07 13:58:26 -0500

    added html amd css to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 function App() {
-  return <div> Hello! </div>;
+  return <div>
+    <h1>My Todos</h1>
+    <div className='card'>
+      <h2>TITLE</h2>
+      <div>
+        <button>Delete</button>
+      </div>
+    </div>
+  </div>;
 }
 
 export default App;
